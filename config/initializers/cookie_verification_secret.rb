a846ace2ae8b8e9f8c44dd08b3692059084039ee
Michael McGuire <mmcgu1966@gmail.com>
2010-07-27 22:20:53 -0700

    creating a new booking System for Coconut Airways

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '1129e19099d5899933a7746929aac5f9a964d8b2eda6c8fcb35b7ef0ae4755b99fcdd38194644e1e91f3e2f3efffc6729e821c258395331c5cc8b16c5523dedf';
